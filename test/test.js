0f55c90cbf3e5142dd577f2bd4cc45930158694a
Vikychahar <viky@gmail.com>
2024-06-28 02:07:43 +0530

    add real unit test using chai mocha

---
+++ test/test.js
@@ -0,0 +1,10 @@
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Sample Test', function() {
+  it('should return true', function() {
+    expect(true).to.be.true;
+  });
+});
+
